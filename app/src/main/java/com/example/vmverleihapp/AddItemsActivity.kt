1d389811c9e4565e8ae4fe9c9edec63440de2726
DoPusti <dominik.pustofka@gmx.net>
2022-08-04 21:16:37 +0200

    4tes Update

--- app/src/main/java/com/example/vmverleihapp/AddItemsActivity.kt
+++ app/src/main/java/com/example/vmverleihapp/AddItemsActivity.kt
@@ -1,11 +1,25 @@
 package com.example.vmverleihapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_add_items.*
+import kotlinx.android.synthetic.main.activity_items.*
 
 class AddItemsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setSupportActionBar(toolbarAddItem)
         setContentView(R.layout.activity_add_items)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbarAddItem.setNavigationOnClickListener {
+            onBackPressed()
+        }
+        fabAddItem.setOnClickListener {
+            val intent = Intent(this@AddItemsActivity,EditProfileActivity::class.java)
+            @Suppress("DEPRECATION")
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/vmverleihapp/ItemsActivity.kt
+++ app/src/main/java/com/example/vmverleihapp/ItemsActivity.kt
@@ -1,18 +1,43 @@
 package com.example.vmverleihapp
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.vmverleihapp.ItemsActivity.Companion.ITEM_ADD_REQUEST_CODE
 import kotlinx.android.synthetic.main.activity_edit_profile.*
 import kotlinx.android.synthetic.main.activity_items.*
 
 class ItemsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_items)
         setSupportActionBar(toolbarItems)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setContentView(R.layout.activity_items)
+         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         toolbarItems.setNavigationOnClickListener {
             onBackPressed()
         }
+        fabAddItem.setOnClickListener {
+            val intent = Intent(this@ItemsActivity,AddItemsActivity::class.java)
+            @Suppress("DEPRECATION")
+            startActivityForResult(intent, ITEM_ADD_REQUEST_CODE)
+        }
+
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        @Suppress("DEPRECATION")
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == ITEM_ADD_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+            } else {
+                Log.e("Activity", "Abgebrochen oder zurück gedrückt")
+            }
+        }
+
+    }
+    companion object {
+        private const val ITEM_ADD_REQUEST_CODE = 3
+
     }
 }
