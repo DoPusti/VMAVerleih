34a5297281160b90f67e27697ed7c87dcff095e4
DoPusti <dominik.pustofka@gmx.net>
2022-08-09 15:48:13 +0200

    Bild wird nun korrekt in Datenbank abgelegt

--- app/src/main/java/com/example/vmverleihapp/AddItemsActivity.kt
+++ app/src/main/java/com/example/vmverleihapp/AddItemsActivity.kt
@@ -2,15 +2,11 @@ package com.example.vmverleihapp
 
 
 import android.app.Activity
-import android.app.AlertDialog
 import android.app.ProgressDialog
-import android.content.ActivityNotFoundException
 import android.content.Intent
-import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import android.provider.Settings
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -20,18 +16,8 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
-import com.karumi.dexter.Dexter
-import com.karumi.dexter.MultiplePermissionsReport
-import com.karumi.dexter.PermissionToken
-import com.karumi.dexter.listener.PermissionRequest
-import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import kotlinx.android.synthetic.main.activity_add_items.*
-import kotlinx.android.synthetic.main.all_items.*
-import kotlinx.android.synthetic.main.all_items.tvimage
-import kotlinx.android.synthetic.main.user_item.*
-import org.json.JSONException
 import java.io.IOException
-import java.text.SimpleDateFormat
 import java.util.*
 
 
@@ -39,7 +25,7 @@ class AddItemsActivity : AppCompatActivity() {
     private lateinit var database: FirebaseDatabase
     private lateinit var referance: DatabaseReference
     private lateinit var firebaseAuth: FirebaseAuth
-    private lateinit var imageURI : Uri
+    private lateinit var imageURI: Uri
     private var firebaseStore: FirebaseStorage? = null
     private var storageReference: StorageReference? = null
 
@@ -63,7 +49,6 @@ class AddItemsActivity : AppCompatActivity() {
             FirebaseDatabase.getInstance("https://vmaverleihapp-default-rtdb.europe-west1.firebasedatabase.app/")
         referance = database.getReference("Users")
         buSave.setOnClickListener {
-            sendData()
             uploadImage()
             val intent = Intent(this@AddItemsActivity, ItemsActivity::class.java)
             @Suppress("DEPRECATION")
@@ -75,41 +60,66 @@ class AddItemsActivity : AppCompatActivity() {
         }
 
     }
+
     private fun launchGallery() {
         val intent = Intent()
         intent.type = "image/*"
         intent.action = Intent.ACTION_GET_CONTENT
         startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST)
     }
-    private fun uploadImage(){
-        if(filePath != null){
-            val ref = storageReference?.child("myImages/" + UUID.randomUUID().toString())
-            val uploadTask = ref?.putFile(filePath!!)
 
-        }else{
+    private fun uploadImage() {
+        if (filePath != null) {
+            val progressDialog = ProgressDialog(this)
+            progressDialog.setMessage("Inserat wird hochgeladen...")
+            progressDialog.setCancelable(false)
+            progressDialog.show()
+
+            val uuid = UUID.randomUUID().toString()
+            val ref = storageReference?.child("myImages/$uuid")
+            val uploadTask = ref?.putFile(filePath!!)
+            ref?.putFile(filePath!!)?.addOnSuccessListener {
+                Toast.makeText(
+                    this@AddItemsActivity,
+                    "Erfolgreich hochgeladen!",
+                    Toast.LENGTH_SHORT
+                ).show()
+                if (progressDialog.isShowing) progressDialog.dismiss()
+            }
+                ?.addOnFailureListener {
+                    if (progressDialog.isShowing) progressDialog.dismiss()
+                    Toast.makeText(
+                        this@AddItemsActivity,
+                        "Fehler beim Hochladen",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            sendData(uuid)
+
+        } else {
             Toast.makeText(this, "Please Upload an Image", Toast.LENGTH_SHORT).show()
         }
     }
 
-    private fun sendData() {
+    private fun sendData(ImageUUID: String) {
         val name = etTitle.text.toString().trim()
         val description = etDescription.text.toString().trim()
         val user = firebaseAuth.currentUser.toString()
         val email = firebaseAuth.currentUser!!.email.toString()
-        val imageUri = ""
         val status = "Verfügbar"
         if (name.isNotEmpty() && description.isNotEmpty()) {
-            val model = DatabaseModel(name, description, user, email, imageUri, status)
+            val model = DatabaseModel(name, description, user, email, ImageUUID, status)
             val id = referance.push().key
             referance.child(id!!).setValue(model)
         } else {
             Toast.makeText(applicationContext, "All Fields Required", Toast.LENGTH_LONG).show()
         }
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (resultCode == Activity.RESULT_OK) {
             if (requestCode == PICK_IMAGE_REQUEST) {
-                if(data == null || data.data == null){
+                if (data == null || data.data == null) {
                     return
                 }
 
@@ -127,13 +137,13 @@ class AddItemsActivity : AppCompatActivity() {
         @Suppress("DEPRECATION")
         super.onActivityResult(requestCode, resultCode, data)
     }
+
     companion object {
         // Codes für Intent onActivityResult
         private const val GALLERY = 1
         private const val CAMERA = 2
         private const val IMAGE = 3
         private const val PICK_IMAGE_REQUEST = 4
-
 
 
     }
