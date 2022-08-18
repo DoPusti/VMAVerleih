dc269d472c8a61ee74e29bd5fc1c597ed0106a23
Daniel <daniel.mantay@trsystems.de>
2022-08-18 13:51:17 +0200

    Unter Chats werden nur noch aktive Chats aufgelistet Ansicht auf letzte Nachricht ergänzt

--- app/src/main/java/com/example/vmverleihapp/ChatsActivity.kt
+++ app/src/main/java/com/example/vmverleihapp/ChatsActivity.kt
@@ -4,19 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Parcelable
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-import com.squareup.picasso.Picasso
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 import kotlinx.android.parcel.Parcelize
+import kotlinx.android.synthetic.main.activity_chat_log.*
 import kotlinx.android.synthetic.main.activity_chats.*
+import kotlinx.android.synthetic.main.activity_chats.toolbar
 import kotlinx.android.synthetic.main.chat_user_row.view.*
+import java.util.*
 
 class ChatsActivity : AppCompatActivity() {
+
+    val adapter = GroupAdapter<GroupieViewHolder>()
+    val userHashMap : HashMap<String, ChatUser> = HashMap<String, ChatUser> ()
+    val latestMessagesHashMap : HashMap<String, LatestMessageItem> = HashMap<String, LatestMessageItem> ()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,38 +32,103 @@ class ChatsActivity : AppCompatActivity() {
         supportActionBar?.setDisplayShowHomeEnabled(true)
         toolbar.setNavigationOnClickListener {onBackPressed()}
 
-        fetchChats()
+        chats_latest_Messages.adapter = adapter
+
+        adapter.setOnItemClickListener {  item, view ->
+            val intent = Intent(view.context, ChatLogActivity::class.java)
+            val chatUserItem = item as LatestMessageItem
+            intent.putExtra(USER_KEY, chatUserItem.user)
+            startActivity(intent)
+
+            //finish()
+        }
+
+
+        getUserData()
+        listenForLatestMessages()
+        //fetchChats()
 
     }
 
-    private fun fetchChats(){
-        val dbRef = FirebaseDatabase.getInstance("https://vmaverleihapp-default-rtdb.europe-west1.firebasedatabase.app/").getReference("Profile")
-        dbRef.addListenerForSingleValueEvent(object: ValueEventListener{
+    private fun refreshRecyclerViewMessages(){
+        adapter.clear()
+        latestMessagesHashMap.values.sortedByDescending { i -> i.timestamp }.forEach {
+            adapter.add(it)
+        }
+    }
 
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val adapter = GroupAdapter<GroupieViewHolder>()
-                for (child in snapshot.children) {
-                    val chat = child.getValue(ChatUser::class.java)
-                    if(chat != null)
-                    {
-                        adapter.add(ChatUserItem(chat))
-                    }
+    private fun listenForLatestMessages(){
 
-                }
-                adapter.setOnItemClickListener {  item, view ->
-                    val intent = Intent(view.context, ChatLogActivity::class.java)
-                    val chatUserItem = item as ChatUserItem
-                    intent.putExtra(USER_KEY, chatUserItem.chatUser)
-                    startActivity(intent)
+        val fromId = FirebaseAuth.getInstance().uid
+        val ref = FirebaseDatabase.getInstance("https://vmaverleihapp-default-rtdb.europe-west1.firebasedatabase.app/").getReference("LatestMessages/$fromId")
 
-                    //finish()
-                }
-                chatsView.adapter = adapter
+        ref.addChildEventListener(object: ChildEventListener {
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                val message = snapshot.getValue(ChatMessage::class.java)
+                setMessage(snapshot.key!!, message)
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                val message = snapshot.getValue(ChatMessage::class.java)
+                setMessage(snapshot.key!!, message)
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
             }
 
             override fun onCancelled(error: DatabaseError) {
-
             }
+
+            private fun setMessage(id: String, message: ChatMessage?)
+            {
+                if (message != null){
+                    val user = userHashMap[message.toId]
+                    if (user != null){
+                        var latestMessage = LatestMessageItem(message.text, message.timestamp,user)
+                        latestMessagesHashMap[id] = latestMessage
+                        refreshRecyclerViewMessages()
+                    }
+                }
+            }
+
+        })
+    }
+
+    private fun getUserData(){
+        val dbRef = FirebaseDatabase.getInstance("https://vmaverleihapp-default-rtdb.europe-west1.firebasedatabase.app/").getReference("Profile")
+        dbRef.addChildEventListener(object: ChildEventListener{
+
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                setUser(snapshot.getValue(ChatUser::class.java))
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                setUser(snapshot.getValue(ChatUser::class.java))
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+                var user = snapshot.getValue(ChatUser::class.java)
+                if (user != null && userHashMap.containsKey(user.id)){
+                    userHashMap.remove(user.id)
+                }
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+
+            private fun setUser(user: ChatUser?)
+            {
+                if (user != null){
+                    userHashMap[user.id] = user
+                }
+            }
+
         })
     }
 
@@ -70,15 +139,16 @@ class ChatsActivity : AppCompatActivity() {
 }
 
 @Parcelize
-class ChatUser(val email: String, val imgUri: String, val id: String) : Parcelable
+class ChatUser(val nachname: String, val imgUri: String, val id: String) : Parcelable
 {
     constructor() : this("","", "")
 }
 
-class ChatUserItem(val chatUser: ChatUser) : Item<GroupieViewHolder>()
+class LatestMessageItem(val latestMessage: String, val timestamp: Long, val user: ChatUser) : Item<GroupieViewHolder>()
 {
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
-        viewHolder.itemView.chat_user_name.text = chatUser.email
+        viewHolder.itemView.chat_user_name.text = user.nachname
+        viewHolder.itemView.chat_latest_Message.text = latestMessage
        // Picasso.get().load(chatUser.imgUri).into(viewHolder.itemView.chat_image)
     }
 
